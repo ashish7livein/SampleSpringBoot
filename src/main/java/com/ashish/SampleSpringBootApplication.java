2f053f2f6a415a6cef0dd5268f413c8ac61728a2
Ashish Ashok <ashishkumar.ashok@IN4LE0055267-MAC.local>
2017-08-02 10:06:07 -0600

    Fea Print out avialable beans and use yml for properties US1

--- src/main/java/com/ashish/SampleSpringBootApplication.java
+++ src/main/java/com/ashish/SampleSpringBootApplication.java
@@ -1,12 +1,25 @@
 package com.ashish;
 
+import java.util.Arrays;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SampleSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+        ApplicationContext ctx = SpringApplication.run(SampleSpringBootApplication.class, args);
+
+        System.out.println("Inspect the beans provided by Spring Boot:");
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println("BEAN: " + beanName);
+        }
+
+    }
 }
